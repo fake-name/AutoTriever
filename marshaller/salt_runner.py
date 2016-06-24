6fff6922eb4ebca6fe57a5ddd2984a9e83aa4690
Fake-Name <something@fake-url.com>
2016-06-24 16:00:07 -0700

    Blurk

--- marshaller/salt_runner.py
+++ marshaller/salt_runner.py
@@ -1,52 +1,112 @@
 
+
+import sys
 import pprint
 import random
+import logging
 
 import salt.cloud
 import salt.client
 import salt.config
 
+import logSetup
 
-def generate_do_conf():
+class VpsHerder(object):
 
-	provider = "do"
-	kwargs = {
-		'image': 'ubuntu-14-04-x64',
-		'size': '512mb',
-		'vm_size': '512mb',
-		'private_networking' : False,
-		'location' : random.choice(['ams2', 'ams3', 'blr1', 'fra1', 'lon1', 'nyc1', 'nyc2', 'nyc3', 'sfo1', 'sgp1', 'tor1']),
+	def __init__(self):
+		self.log = logging.getLogger("Main.VpsHerder")
+		self.local = salt.client.LocalClient()
+		self.cc = salt.cloud.CloudClient('/etc/salt/cloud')
 
 
-	}
-	return provider, kwargs
+	def generate_do_conf(self):
 
-def instantiate_minion():
-	cc = salt.cloud.CloudClient('/etc/salt/cloud')
+		provider = "do"
+		kwargs = {
+			'image': 'ubuntu-14-04-x64',
+			'size': '512mb',
+			'vm_size': '512mb',
+			'private_networking' : False,
+			'location' : random.choice(['ams2', 'ams3', 'blr1', 'fra1', 'lon1', 'nyc1', 'nyc2', 'nyc3', 'sfo1', 'sgp1', 'tor1']),
 
 
-	# provider, kwargs = generate_do_conf()
-	# print("Creating instance...")
-	# instance = cc.create(names=['test-1'], provider=provider, **kwargs)
+		}
+		return provider, kwargs
 
-	# # print("Instance: ", instance)
-	# with open("minion.txt", "w") as fp:
-	# 	fp.write(pprint.pformat(instance))
-	# with open("minion_raw.txt", "w") as fp:
-	# 	fp.write(str(instance))
+	def generate_conf(self):
+		# TODO: Vultr goes here too
+		return random.choice([self.generate_do_conf])()
 
-	cc.destroy('test-1')
-	# images = cc.list_images()
-	# locs   = cc.list_locations()
-	# sizes  = cc.list_sizes()
-	# pprint.pprint(images)
-	# pprint.pprint(locs)
-	# pprint.pprint(sizes)
+	def make_client(self, clientname):
 
-def dump_minion_conf():
-	conf = salt.config.client_config('/etc/salt/minion')
-	print(conf)
+		provider, kwargs = self.generate_conf()
+		self.log.info("Creating instance...")
+		self.log.info("	Client name: '%s'", clientname)
+		self.log.info("	using provider: '%s'", provider)
+		self.log.info("	kwargs: '%s'", kwargs)
+		ret = self.cc.create(names=[clientname], provider=provider, **kwargs)
+		self.log.info("Instance created!")
+		# instance = cc.create(names=['test-1'], provider=provider, **kwargs)
+		# print(ret)
+
+
+
+
+
+	def configure_client(self, clientname):
+		assert "_" not in clientname, "VM names cannot contain _ on digital ocean, I think?"
+		self.log.info("Configuring client")
+
+		commands = [
+			['cmd.run', ["mkdir -p /scraper", ],      {}],
+			['cmd.run', ["apt-get install -y build-essential git", ],      {}],
+			['cmd.run', ["git clone https://github.com/fake-name/AutoTriever.git /scraper"], {}],
+			['cmd.run', ["ls /scraper", ], {}],
+			['cmd.run', ["whoami", ], {}],
+
+			# Make swap so
+			['cmd.run', ["dd if=/dev/zero of=/swapfile bs=1M count=1024", ], {}],
+			['cmd.run', ["mkswap /swapfile", ], {}],
+			['cmd.run', ["swapon /swapfile", ], {}],
+			['cmd.run', ["./run.sh", ], {"cwd" : '/scraper'}],
+		]
+
+		for command, args, kwargs in commands:
+			self.log.info("Executing command '%s', args: '%s', kwargs: '%s'", command, args, kwargs)
+			resp = self.local.cmd(clientname,
+				fun=command,
+				arg=args,
+				kwarg=kwargs
+				)
+			assert clientname in resp
+			if resp[clientname]:
+				for line in resp[clientname].split("\n"):
+					self.log.info("	%s", line)
+			else:
+				self.log.info("Received empty response")
+
+
+
+	def destroy_client(self, clientname):
+		cc = salt.cloud.CloudClient('/etc/salt/cloud')
+
+		cc.destroy(clientname)
+		# images = cc.list_images()
+		# locs   = cc.list_locations()
+		# sizes  = cc.list_sizes()
+		# pprint.pprint(images)
+		# pprint.pprint(locs)
+		# pprint.pprint(sizes)
+
+	def dump_minion_conf(self):
+		conf = salt.config.client_config('/etc/salt/minion')
+		self.log.info(conf)
 
 if __name__ == '__main__':
-	instantiate_minion()
-	# dump_minion_conf()
+	logSetup.initLogging()
+	herder = VpsHerder()
+	if "destroy" in sys.argv:
+		herder.destroy_client("test-1")
+	else:
+		# herder.make_client("test-1")
+		herder.configure_client("test-1")
