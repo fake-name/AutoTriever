7159e0d35f073e40b7df5f603ff9f349cc6b5f73
Fake-Name <something@fake-url.com>
2016-07-08 16:50:26 -0700

    Apparently args has to be none if you have no args.

--- deps/logSetup.py
+++ deps/logSetup.py
@@ -67,7 +67,8 @@ class ColourHandler(logging.Handler):
 		else:
 			record.style = clr.Style.NORMAL
 
-		record.args = [str(argsf).encode("utf-8", "replace").decode("utf-8") for argsf in record.args]
+		if record.args:
+			record.args = [str(argsf).encode("utf-8", "replace").decode("utf-8") for argsf in record.args]
 
 		record.padding = ""
 		print((self.format(record)))
