8c7a1edde512683c4a70379a93ef6feadb6c39bf
Fake-Name <something@fake-url.com>
2019-07-17 23:33:03 -0700

    Fix duplicate import warnings by moving things out of __init__

---
+++ autotriever/modules/WebRequest/wr_module.py
@@ -0,0 +1,37 @@
+
+import WebRequest
+
+class PluginInterface_WebRequest(WebRequest.WebGetRobust):
+
+	name = 'WebRequest'
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+
+		self.calls = {
+			'getpage'                  : self.getpage,
+			'getItem'                  : self.getItem,
+			'getHead'                  : self.getHead,
+			'getFileAndName'           : self.getFileAndName,
+			'addCookie'                : self.addCookie,
+			'addSeleniumCookie'        : self.addSeleniumCookie,
+			'stepThroughCloudFlare'    : self.stepThroughCloudFlare,
+
+			'chromiumGetRenderedItem'  : self.chromiumGetRenderedItem,
+			'getHeadChromium'          : self.getHeadChromium,
+			'getHeadTitleChromium'     : self.getHeadTitleChromium,
+			'getItemChromium'          : self.getItemChromium,
+		}
+
+	# def test(self):
+	# 	print("Exec()ing `runTest.sh` from directory root!")
+
+	# 	import subprocess
+	# 	command = "./runTests.sh"
+
+	# 	process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+	# 	output, error = process.communicate()
+
+	# 	print("Command output: ", output)
+	# 	print("Command errors: ", error)
