79d61c7c01763f747b3d4c11bad119a9440c8eb3
Fake-Name <something@fake-url.com>
2019-08-23 23:06:48 -0700

    Moar chromium rendering. If your text-based-site uses vue.js, I hate you.

--- autotriever/modules/SmartFetch/Processors/PreemptProcessor_ChromeRender.py
+++ autotriever/modules/SmartFetch/Processors/PreemptProcessor_ChromeRender.py
@@ -30,8 +30,6 @@ class FoxTellerFetch(PreemptProcessorBase.PreemptProcessorBase):
 		return soup.prettify()
 
 
-
-
 	def premptive_handle_content(self, url):
 		'''
 		Get content with bullshit JS rendering.
@@ -40,6 +38,52 @@ class FoxTellerFetch(PreemptProcessorBase.PreemptProcessorBase):
 		wrapper_step_through_timeout = 60
 		loading_str                  = "Chapter loading..."
 
+		with self.wg._chrome_context(url, extra_tid=False) as cr:
+			self.wg._syncIntoChromium(cr)
+			try:
+
+				response = cr.blocking_navigate_and_get_source(url)
+
+				raw_url = cr.get_current_url()
+				fileN = urllib.parse.unquote(urllib.parse.urlparse(raw_url)[2].split("/")[-1])
+				fileN = bs4.UnicodeDammit(fileN).unicode_markup
+
+				# Short circuit for the binary content case.
+				if response['binary']:
+					return response['content'], fileN, magic.from_buffer(response['content'], mime=True)
+
+				self.log.info("Waiting for content to render...")
+
+				for _ in range(wrapper_step_through_timeout):
+					body = cr.get_rendered_page_source()
+					if loading_str not in body:
+						self.log.info("Content appears to have rendered!")
+						return self.de_garbage_html(body), fileN, "text/html"
+					time.sleep(1)
+
+			finally:
+				self.wg._syncOutOfChromium(cr)
+
+		raise WebRequest.GarbageSiteWrapper("Could not render JS content!")
+
+
+
+class MeeJeeFetch(PreemptProcessorBase.PreemptProcessorBase):
+
+	log_name = "Main.Processor.MeeJee"
+
+	@staticmethod
+	def preemptive_wants_url(lowerspliturl:tuple):
+		return lowerspliturl.netloc.endswith("meejee.net")
+
+
+	def premptive_handle_content(self, url):
+		'''
+		Fuck Vue.js so much
+		'''
+
+		wrapper_step_through_timeout = 60
+		loading_str                  = "{{"
 
 		with self.wg._chrome_context(url, extra_tid=False) as cr:
 			self.wg._syncIntoChromium(cr)
