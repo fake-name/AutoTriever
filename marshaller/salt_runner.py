dc3774439a2451b13e8897ecc10473c2618bbb67
Fake-Name <something@fake-url.com>
2020-10-02 04:02:59 -0700

    Ugh, Fuck how ubuntu gimps python distutils so much.

--- marshaller/salt_runner.py
+++ marshaller/salt_runner.py
@@ -551,7 +551,7 @@ class VpsHerder(object):
 
 			# Apparently at least one VPS host has separated git from build-essential?
 			['pkg.install', ['build-essential', 'locales', 'git', 'libfontconfig', 'wget', 'htop', 'libxml2', 'libxslt1-dev',
-				'python3-dev', 'python3-dbg', 'libz-dev', 'curl', 'screen'],                                                                      {}, None],
+				'python3-dev', 'python3-dbg', 'python3-distutils', 'libz-dev', 'curl', 'screen'],                                                                      {}, None],
 			['pkg.install', ['libasound2', 'libatk1.0-0', 'libc6', 'libcairo2', 'libcups2', 'libdbus-1-3', 'libexpat1', 'libfontconfig1', 'libgcc1',
 					'libgconf-2-4', 'libgdk-pixbuf2.0-0', 'libglib2.0-0', 'libgtk-3-0', 'libnspr4', 'libpango-1.0-0', 'libpangocairo-1.0-0', 'libstdc++6',
 					'libx11-6', 'libx11-xcb1', 'libxcb1', 'libxcursor1', 'libxdamage1', 'libxext6', 'libxfixes3', 'libxi6', 'libxrandr2', 'libxrender1',
